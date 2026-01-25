de55492c989154527ccde40c75e12f335fc2f479
fewolf123 <fewolf123@gmail.com>
2026-01-25 22:36:27 +0100

    fixes

--- components/presentation/slide-led-backgrounds.tsx
+++ components/presentation/slide-led-backgrounds.tsx
@@ -13,9 +13,9 @@ export function SlideLedBackgrounds({ isActive, images }: SlideLedBackgroundsPro
 
   return (
     <section className="relative h-full w-full overflow-hidden bg-black">
-      <div className="absolute inset-0 grid grid-cols-2 grid-rows-2 gap-4 p-6">
+      <div className="absolute inset-0 grid grid-cols-2 grid-rows-2 gap-0 p-6 bg-black">
         {images.map((src, index) => (
-          <div key={src} className="relative h-full min-h-[140px] w-full overflow-hidden">
+          <div key={src} className="relative h-full min-h-[140px] w-full overflow-hidden bg-black">
             <Image
               src={getAssetPath(src)}
               alt={`LED Hintergrund ${index + 1}`}
